acef8723ed6699811ff0800c35cc51259a4ab3b3
Daniel Valdivia <daniel@freeagentsoftware.com>
2016-10-31 20:15:57 -0700

    make export single line insert

--- src/org/mediawiki/importer/SqlWriter.java
+++ src/org/mediawiki/importer/SqlWriter.java
@@ -60,7 +60,7 @@ public abstract class SqlWriter implements DumpWriter {
 			return new SqlLiteral("RAND()");
 		}
 		public boolean supportsMultiRowInsert() {
-			return true;
+			return false;
 		}
 		public String getTextTable() {
 			return "text";
